bde1bec835e04d4a3919da7390ce026dcadbbbdb
tuanduc8t1 <166401438+tuanduc8t1@users.noreply.github.com>
2024-04-19 20:11:26 +0700

    Delete routes directory

--- routes/client/home.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const controller = require("../../controllers/client/home.controller");
-
-router.get("/", controller.index);
-
-module.exports = router;
--- routes/client/index.route.js
+++
@@ -1,8 +0,0 @@
-const homeRoutes = require("./home.route");
-const productRoutes = require("./product.route");
-
-module.exports = (app) => {
-  app.use("/", homeRoutes);
-  
-  app.use("/products", productRoutes);
-}
