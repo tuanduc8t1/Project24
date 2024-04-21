d9cc7a20249308b16882ac56874d76f880fb838e
tuanduc8t1 <166401438+tuanduc8t1@users.noreply.github.com>
2024-04-21 16:18:01 +0700

    Add files via upload

---
+++ routes/client/index.route.js
@@ -0,0 +1,12 @@
+const homeRoutes = require("./home.route");
+const productRoutes = require("./product.route");
+
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+
+module.exports = (app) => {
+  app.use(categoryMiddleware.category);
+
+  app.use("/", homeRoutes);
+  
+  app.use("/products", productRoutes);
+}
