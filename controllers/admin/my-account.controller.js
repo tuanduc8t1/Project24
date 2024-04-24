c8bba9127824f77e6a5e96a8e94354525069e482
tuanduc8t1 <166401438+tuanduc8t1@users.noreply.github.com>
2024-04-24 20:48:19 +0700

    Delete controllers directory

--- controllers/admin/my-account.controller.js
+++
@@ -1,6 +0,0 @@
-// [GET] /admin/my-account/
-module.exports.index = (req, res) => {
-  res.render("admin/pages/my-account/index", {
-    pageTitle: "Thông tin cá nhân"
-  });
-}
--- controllers/client/home.controller.js
+++
@@ -1,6 +0,0 @@
-// [GET] /
-module.exports.index = (req, res) => {
-  res.render("client/pages/home/index", {
-    pageTitle: "Trang chủ",
-  });
-}
