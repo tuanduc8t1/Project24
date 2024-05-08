541e69afeb3a3ef7865bbe98aa0ea845be9e2796
tuanduc8t1 <166401438+tuanduc8t1@users.noreply.github.com>
2024-05-08 20:27:46 +0700

    Delete middlewares directory

--- middlewares/client/setting.middleware.js
+++
@@ -1,9 +0,0 @@
-const SettingGeneral = require("../../models/setting-general.model");
-
-module.exports.settingsGeneral = async (req, res, next) => {
-  const settingsGeneral = await SettingGeneral.findOne({});
-
-  res.locals.settingsGeneral = settingsGeneral;
-
-  next();
-}
