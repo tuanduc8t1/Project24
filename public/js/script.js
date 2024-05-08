c0c34d96d791db87af1f4fcd503f564bd2b47b89
tuanduc8t1 <166401438+tuanduc8t1@users.noreply.github.com>
2024-05-08 20:33:32 +0700

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,33 @@
+// show-alert
+const showAlert = document.querySelector("[show-alert]");
+if(showAlert) {
+  let time = showAlert.getAttribute("data-time");
+  time = parseInt(time);
+
+  // Sau time giây sẽ đóng thông báo
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden");
+  }, time);
+
+  // Khi click vào nút close-alert sẽ đóng luôn
+  const closeAlert = showAlert.querySelector("[close-alert]");
+  closeAlert.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden");
+  });
+}
+// End show-alert
+
+// Update Cart
+const tableCart = document.querySelector("[table-cart]");
+if(tableCart) {
+  const listInputQuantity = tableCart.querySelectorAll("input[name='quantity']");
+  listInputQuantity.forEach(input => {
+    input.addEventListener("change", () => {
+      const quantity = input.value;
+      const productId = input.getAttribute("item-id");
+
+      window.location.href = `/cart/update/${productId}/${quantity}`;
+    });
+  });
+}
+// End Update Cart
---
+++ routes/admin/account.route.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const multer  = require('multer');
+const router = express.Router();
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
+const controller = require("../../controllers/admin/account.controller");
+
+const validate = require("../../validates/admin/account.validate");
+
+const upload = multer();
+
+router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+  "/create",
+  upload.single('avatar'),
+  uploadCloud.uploadSingle,
+  validate.createPost,
+  controller.createPost
+);
+
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+  "/edit/:id",
+  upload.single('avatar'),
+  uploadCloud.uploadSingle,
+  validate.editPatch,
+  controller.editPatch
+);
+
+module.exports = router;
