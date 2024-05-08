debd8f7b0dab9d4dfffdfca2900ab68c50116756
tuanduc8t1 <166401438+tuanduc8t1@users.noreply.github.com>
2024-05-08 20:29:05 +0700

    Delete routes directory

--- routes/admin/my-account.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const controller = require("../../controllers/admin/my-account.controller");
-
-router.get("/", controller.index);
-
-module.exports = router;
--- routes/client/search.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const controller = require("../../controllers/client/search.controller");
-
-router.get("/", controller.index);
-
-module.exports = router;
